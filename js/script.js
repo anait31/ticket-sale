7e0367ca0db9ba8c5c34dd6bf84a9cab443c3526
anait07 <anaitullah198@gmail.com>
2024-02-18 02:34:14 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const seats = document.querySelectorAll('.seat-div');
+
+// console.log(seats)
+for (let seat of seats) {
+    seat.addEventListener('click', function() {
+        // console.log(seat);
+        const seatName = seat.querySelectora
+    });
+}
